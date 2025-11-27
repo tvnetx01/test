7c4ab161380df5f3b2077434b01d8c7fabcefae1
tvnetx01 <tvnetx01@gmail.com>
2025-11-28 01:01:51 +0800

    Update kode.js

--- kode.js
+++ kode.js
@@ -1,6 +1,6 @@
 {
   "kode": [
-    "8d969eef6ecad3c29a3a629280e686cff8fabceff162e16a0baf3e3d0f8aab5a",
-    "c4603f85772a79e39ca14e4472c3f6b0db5405cb9184160a95cace0f1a4acf17"
+    "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92",
+    "481f6cc0511143ccdd7e2d1b1b94faf0a700a8b49cd13922a70b5ae28acaa8c5"
   ]
 }
