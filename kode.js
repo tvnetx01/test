8096ce99b2cbcd0f49a03fd6e07ec937366ad09a
tvnetx01 <tvnetx01@gmail.com>
2025-11-28 00:50:14 +0800

    Update kode.js

--- kode.js
+++ kode.js
@@ -1,6 +1,6 @@
 {
   "kode": [
-    "8d969eef6ecad3c29a3a629280e686cf",  // hash dari "123456"
-    "e10adc3949ba59abbe56e057f20f883e"   // hash dari "12345678"
+    "8d969eef6ecad3c29a3a629280e686cff8fabce1234567890abcdef1234567890", 
+    "6c569aabbf7775ef8fc570e228c16b981f7c1234567890abcdef1234567890ab"
   ]
 }
